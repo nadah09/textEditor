d6740149e0c416d9b7553c5de49e47e3a42b1cd0
nada <nhussein@mit.edu>
2021-09-04 00:33:30 -0400

    added create and switch docs

--- main.py
+++ main.py
@@ -28,18 +28,31 @@ def edit_document(doc, txt):
 def setup_document(name):
     doc = d.Document()
     return doc
+
+
     
 
 
 
 if __name__=="__main__":
     doc = setup_document("doc1")
+    docs = {}
+    docs["doc1"] = doc
     print("Welcome to Text Editor! To quit, type 'QUIT'.")
     while True:
         txt = input("Enter a new command: ")
-
         if txt == "QUIT":
             break
+        if txt.split(" ")[0] == "CREATE":
+            name = txt.split(" ")[1]
+            if name not in docs:
+                doc = setup_document(name)
+                docs[name] = doc   
+        if txt.split(" ")[0] == "SWITCH":
+            name = txt.split(" ")[1]
+            if name in docs:
+                doc = docs[name]
+                print(doc.text)
         else:
             edit = edit_document(doc, txt)
             print(edit)
